0280ccc4d58d920d42c9574b7108c5623389b710
KIRUBHANANDHAN <viranandhu@gmail.com>
2018-05-29 03:09:12 -0700

    concatenation

---
+++ src/com/basic/DataTypeExample.java
@@ -0,0 +1,8 @@
+package com.basic;
+public class DataTypeExample {
+		public static void main(String args[])
+		{
+ String a="baby(kirubhanandhan) ";
+ String b="is laughing";
+ System.out.println(a+b);
+}}
