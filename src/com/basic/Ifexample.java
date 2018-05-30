211a1cbb6edf434932809e7558415d0f1bcb207d
KIRUBHANANDHAN <viranandhu@gmail.com>
2018-05-29 21:24:44 -0700

    example for if else

---
+++ src/com/basic/Ifexample.java
@@ -0,0 +1,13 @@
+package com.basic;
+public class Ifexample {
+	public static void main(String args[])
+	{
+	String s1="hai";
+	String s2="hello";
+	if(s1==s2)
+		System.out.print("kirubha");
+	else
+		System.out.print("vira");;;;;
+	}
+
+}
