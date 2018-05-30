1de04dfef775a2976407949c385454f10c3a50fd
KIRUBHANANDHAN <viranandhu@gmail.com>
2018-05-29 21:31:10 -0700

    example for ternary

---
+++ src/com/basic/Ternary.java
@@ -0,0 +1,12 @@
+package com.basic;
+
+public class Ternary {
+
+	public static void main(String[] args) {
+		String s1="hai";
+		String s2="hello";
+		String s3=(s1==s2)?"kirubha":"vira";
+		System.out.print(s3);
+	}
+
+}
